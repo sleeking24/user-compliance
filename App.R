c57da5963a3a74c0140928ebaf54fe7e4acdbc71
sleeking24 <sofiaking2015@u.northwestern.edu>
2017-12-18 16:58:00 -0600

    Update App.R

--- App.R
+++ App.R
@@ -1,12 +1,7 @@
 library(shiny)
-library(civis)
 
-query<-(sql("select primary_organization_name as Organization_Name, primary_organization_status as Status, org_type, sum(case when max = 3 then 1 else 0 end) as Report_Viewer_Count, sum(case when max =6 then 1 else 0 end) as Analyst_Count, max_report_users as Max_Report_Viewers, max_analyst_users as Max_Analysts
-            from scratch.usercompliance
-            group by 1,2,3,6,7
-            order by 1"))
-uc<-read_civis(query,database="redshift-general")
-uc
+load(file='uc.Rdata')
+# uc
 names<-uc[[1]]
 
 ui<-fluidPage(
