4c98425fbe3fe48601cf9f80eb5e41793a6e2d58
sleeking24 <sofiaking2015@u.northwestern.edu>
2017-12-19 15:26:41 -0600

    Update install.R

--- install.R
+++ install.R
@@ -5,10 +5,4 @@ install.packages(c('civis',
 
 library(civis)
 
-query<-(sql("select primary_organization_name as Organization_Name, primary_organization_status as Status, org_type, sum(case when max = 3 then 1 else 0 end) as Report_Viewer_Count, sum(case when max =6 then 1 else 0 end) as Analyst_Count, max_report_users as Max_Report_Viewers, max_analyst_users as Max_Analysts
-            from scratch.usercompliance
-            group by 1,2,3,6,7
-            order by 1"))
-uc<-read_civis(query,database="redshift-general")
 
-save(uc,"app/uc.Rdata")
