c295d289db598ec218979013a5c4cb99549922c3
sleeking24 <sofiaking2015@u.northwestern.edu>
2017-12-19 16:25:49 -0600

    This worked locally 12.19

--- app.R
+++ app.R
@@ -1,13 +1,21 @@
+install.packages(c('civis',
+                   'shiny'),
+                 repos='https://cran.rstudio.com/')
 
-library (civis)
-library (shiny)
+library(shiny)
+library(civis)
 
-load("app/uc.Rdata")
-# uc
+query<-(sql("select primary_organization_name as Organization_Name, primary_organization_status as Status, org_type, sum(case when max = 3 then 1 else 0 end) as Report_Viewer_Count, sum(case when max =6 then 1 else 0 end) as Analyst_Count, max_report_users as Max_Report_Viewers, max_analyst_users as Max_Analysts
+            from scratch.usercompliance
+            group by 1,2,3,6,7
+            order by 1"))
+uc<-read_civis(query,database="redshift-general")
+uc
 names<-uc[[1]]
 
 ui<-fluidPage(
   tags$h1("User Compliance Report"),
+  tags$em("by Sofia King "),
   selectInput("grp","Choose a Org",choices=names),
   DT::dataTableOutput("data")
 )
