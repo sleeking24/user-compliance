ed14c02c7c32fba77ba110e53dfc8bcf3783dc8d
sleeking24 <sofiaking2015@u.northwestern.edu>
2018-02-16 14:31:19 -0600

    Update global.R

--- global.R
+++ global.R
@@ -4,6 +4,9 @@ install.packages(c('civis',
                  repos='https://cran.rstudio.com/')
 
 library(civis)
+library(shiny)
+library(DT)
+
 
 query<-(sql("select primary_organization_name as Organization_Name, primary_organization_status as Status, org_type, sum(case when max = 3 then 1 else 0 end) as Report_Viewer_Count, sum(case when max =6 then 1 else 0 end) as Analyst_Count, max_report_users as Max_Report_Viewers, max_analyst_users as Max_Analysts, sum(case when max = 3 and active_user =1 then 1 else 0 end) as Active_Report_Viewer_Count, sum(case when max =6 and active_user = 1 then 1 else 0 end) as Active_Analyst_Count
 from scratch.usercompliance
