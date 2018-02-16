cd39656a956db3b23774966f2e0670305d16084a
sleeking24 <sofiaking2015@u.northwestern.edu>
2018-02-16 16:25:21 -0600

    add session to function

--- app.R
+++ app.R
@@ -25,7 +25,7 @@ ui<-fluidPage(
 )
 
 
-server<-function(input,output){
+server<-function(input,output,session){
   output$data<-DT::renderDataTable(uc[uc$organization_name == input$grp, ])
   
  session$allowReconnect("force")}
